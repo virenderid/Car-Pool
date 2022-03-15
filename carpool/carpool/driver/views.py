4f2d66e5c374ce66e26da310d16039dd30d7e139
virender <virenderkr@hotmail.com>
2022-03-15 18:25:59 +0000

    update-migrate

--- carpool/carpool/driver/views.py
+++ carpool/carpool/driver/views.py
@@ -8,7 +8,7 @@ from django.http import JsonResponse
 from rider.models import ride
 from django.core import serializers
 
-#import numpy as np
+import numpy as np
 #import googlemaps 
 import json
 
